fcd4392f91579480fa5efaf582759d94b1db88ea
saroz rimal <sarojrimal>
2019-07-11 16:56:27 +0545

    required files are uploaded

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports ={
+
+    client:'mysql',
+    connection:{
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'onlinestore'
+
+    
+},
+migrations: {
+    tableName:'migrations',
+    directory: path.resolve(__dirname,'./migrations'),
+},
+useNullAsDefault:true
+};
